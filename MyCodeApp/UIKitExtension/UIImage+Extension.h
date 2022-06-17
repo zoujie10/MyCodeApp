437de0210544c0546566f1bacbdc4faaf98b7dd4
zoujie <zj89011810@163.com>
2022-06-17 14:34:01 +0800

    button extension

---
+++ MyCodeApp/UIKitExtension/UIImage+Extension.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+Extension.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/6/17.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (Extension)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/UIKitExtension/UIImage+Extension.m
@@ -0,0 +1,12 @@
+//
+//  UIImage+Extension.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/6/17.
+//
+
+#import "UIImage+Extension.h"
+
+@implementation UIImage (Extension)
+
+@end
