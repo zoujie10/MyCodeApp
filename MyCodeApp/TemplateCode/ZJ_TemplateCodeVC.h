c67d07a1d127b44a2b51b074859a25a0bd887886
zoujie <zj89011810@163.com>
2022-06-24 20:45:12 +0800

    template

---
+++ MyCodeApp/TemplateCode/ZJ_TemplateCodeVC.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_TemplateCodeVC.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/6/24.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_TemplateCodeVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/TemplateCode/ZJ_TemplateCodeVC.m
@@ -0,0 +1,31 @@
+//
+//  ZJ_TemplateCodeVC.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/6/24.
+//
+
+#import "ZJ_TemplateCodeVC.h"
+
+@interface ZJ_TemplateCodeVC ()
+
+@end
+
+@implementation ZJ_TemplateCodeVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
