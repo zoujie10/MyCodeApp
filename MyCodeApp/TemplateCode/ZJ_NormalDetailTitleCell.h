5b150c41419cd156355624b3021e8131b70c2542
zoujie <zj89011810@163.com>
2022-06-25 09:45:10 +0800

    comment

---
+++ MyCodeApp/TemplateCode/ZJ_NormalDetailTitleCell.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_NormalDetailTitleCell.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/6/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_NormalDetailTitleCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
