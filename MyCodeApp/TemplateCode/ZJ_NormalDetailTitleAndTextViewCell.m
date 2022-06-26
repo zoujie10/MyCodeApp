fcc5f79bdcadf1b4bebbfe881d28fdeaf0dafff7
zoujie <zj89011810@163.com>
2022-06-26 20:00:59 +0800

    comment

---
+++ MyCodeApp/TemplateCode/ZJ_NormalDetailTitleAndTextViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ZJ_NormalDetailTitleAndTextViewCell.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/6/25.
+//
+
+#import "ZJ_NormalDetailTitleAndTextViewCell.h"
+
+@implementation ZJ_NormalDetailTitleAndTextViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
