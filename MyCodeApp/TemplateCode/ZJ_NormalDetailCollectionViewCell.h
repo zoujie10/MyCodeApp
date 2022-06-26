a0a0f0ef41f612bdad40a0b6eb1390fe528eef2f
zoujie <zj89011810@163.com>
2022-06-26 19:58:27 +0800

    comment

---
+++ MyCodeApp/TemplateCode/ZJ_NormalDetailCollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_NormalDetailCollectionViewCell.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/6/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_NormalDetailCollectionViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
