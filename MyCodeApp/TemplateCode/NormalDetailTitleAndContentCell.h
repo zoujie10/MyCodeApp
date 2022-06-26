f6f800786df2ece19ff0dc997bd34b3dd8fdada4
zoujie <zj89011810@163.com>
2022-06-26 19:53:29 +0800

    comment

---
+++ MyCodeApp/TemplateCode/NormalDetailTitleAndContentCell.h
@@ -0,0 +1,16 @@
+//
+//  NormalDetailTitleAndContentCell.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/6/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NormalDetailTitleAndContentCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/TemplateCode/NormalDetailTitleAndContentCell.m
@@ -0,0 +1,23 @@
+//
+//  NormalDetailTitleAndContentCell.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/6/25.
+//
+
+#import "NormalDetailTitleAndContentCell.h"
+
+@implementation NormalDetailTitleAndContentCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
