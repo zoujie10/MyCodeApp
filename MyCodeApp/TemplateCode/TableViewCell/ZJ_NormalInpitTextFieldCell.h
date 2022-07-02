194562ab7b7fda2b71b5f8848cc2826ae4138937
zoujie <zj89011810@163.com>
2022-07-02 14:45:38 +0800

    header view

---
+++ MyCodeApp/TemplateCode/TableViewCell/ZJ_NormalInpitTextFieldCell.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_NormalInpitTextFieldCell.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/2.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_NormalInpitTextFieldCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/TemplateCode/TableViewCell/ZJ_NormalInpitTextFieldCell.m
@@ -0,0 +1,23 @@
+//
+//  ZJ_NormalInpitTextFieldCell.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/2.
+//
+
+#import "ZJ_NormalInpitTextFieldCell.h"
+
+@implementation ZJ_NormalInpitTextFieldCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
