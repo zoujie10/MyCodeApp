95b68f6c986fa5ba998c4efbbda0d288dbd08397
zoujie <zj89011810@163.com>
2022-07-04 16:22:30 +0800

    input

---
+++ MyCodeApp/TemplateCode/ZJ_Custom_WorkFlow.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_Custom_WorkFlow.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/4.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_Custom_WorkFlow : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/TemplateCode/ZJ_Custom_WorkFlow.m
@@ -0,0 +1,12 @@
+//
+//  ZJ_Custom_WorkFlow.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/4.
+//
+
+#import "ZJ_Custom_WorkFlow.h"
+
+@implementation ZJ_Custom_WorkFlow
+
+@end
