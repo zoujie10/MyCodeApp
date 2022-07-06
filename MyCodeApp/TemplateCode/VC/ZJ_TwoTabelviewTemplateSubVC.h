357c0f51a1f3a8fddae58195118ecfa51609040a
zoujie <zj89011810@163.com>
2022-07-06 21:42:40 +0800

    vc

---
+++ MyCodeApp/TemplateCode/VC/ZJ_TwoTabelviewTemplateSubVC.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_TwoTabelviewTemplateSubVC.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/5.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_TwoTabelviewTemplateSubVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/TemplateCode/VC/ZJ_TwoTabelviewTemplateVC.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_TwoTabelviewTemplateVC.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/5.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_TwoTabelviewTemplateVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
