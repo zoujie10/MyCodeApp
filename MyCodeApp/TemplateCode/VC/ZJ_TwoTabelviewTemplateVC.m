0bae57ed11a8d260aedbfc1f0069f860c46c0d13
zoujie <zj89011810@163.com>
2022-07-07 20:47:31 +0800

    vc

--- MyCodeApp/TemplateCode/VC/ZJ_TwoTabelviewTemplateVC.m
+++ MyCodeApp/TemplateCode/VC/ZJ_TwoTabelviewTemplateVC.m
@@ -8,12 +8,13 @@
 #import "ZJ_TwoTabelviewTemplateVC.h"
 #import "ZJ_TwoTabelviewTemplateSubVC.h"
 static CGFloat topGapHeight = 10;//导航栏下方留白空隙
+static NSString *cellId = @"CellSubScrollView";
 
 @interface ZJ_TwoTabelviewTemplateVC ()<UITableViewDelegate,UITableViewDataSource>
 ///主tableView
 @property(nonatomic,strong) UITableView *mainTableView;
 
-@property(nonatomic,strong) ZJ_TwoTabelviewTemplateSubVC *foretasteProductsSubListView;
+@property(nonatomic,strong) ZJ_TwoTabelviewTemplateSubVC *subListView;
 @end
 
 @implementation ZJ_TwoTabelviewTemplateVC
@@ -44,5 +45,59 @@ static CGFloat topGapHeight = 10;//导航栏下方留白空隙
 
 	self.mainTableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
 	
+}
+#pragma mark delegate
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+	return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+	return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+	 UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+	 if (!cell) {
+		  cell =
+		  [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+								 reuseIdentifier:cellId];
+		  [cell.contentView addSubview:self.subListView.view];
+		  cell.backgroundColor = [UIColor whiteColor];
+		  cell.selectionStyle = UITableViewCellSelectionStyleNone;
+	 }
+	return cell;
+}
+/**
+ UITableViewDelegate
+ */
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+	CGFloat rowHeight = self.subListView.view.bounds.size.height;
+	return rowHeight;
+}
+
+#pragma mark lazyload
+-(UITableView *)mainTableView{
+	if (!_mainTableView) {
+		_mainTableView =
+		[[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+		_mainTableView.showsVerticalScrollIndicator = NO;
+		_mainTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+		//设置代理
+		_mainTableView.delegate = self;
+		_mainTableView.dataSource = self;
+		_mainTableView.bounces = NO;
+	}
+	return _mainTableView;
+}
+
+
+
+- (ZJ_TwoTabelviewTemplateSubVC *)subListView{
+	if(!_subListView){
+		_subListView = [[ZJ_TwoTabelviewTemplateSubVC alloc] init];
+		[self addChildViewController:_subListView];
+		_subListView.view.backgroundColor = [UIColor whiteColor];
+	}
+	return _subListView;
 }
 @end
