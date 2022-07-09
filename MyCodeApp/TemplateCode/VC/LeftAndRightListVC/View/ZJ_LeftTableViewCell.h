bf28fd15bed5234ef3b3bfd406a11501e18ce588
zoujie <zj89011810@163.com>
2022-07-09 17:37:53 +0800

    vc

---
+++ MyCodeApp/TemplateCode/VC/LeftAndRightListVC/View/ZJ_LeftTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  ZJ_LeftTableViewCell.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/9.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_LeftTableViewCell : UITableViewCell
+@property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UIView *lineView;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/TemplateCode/VC/LeftAndRightListVC/View/ZJ_LeftTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ZJ_LeftTableViewCell.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/9.
+//
+
+#import "ZJ_LeftTableViewCell.h"
+
+@implementation ZJ_LeftTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
