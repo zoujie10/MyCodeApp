a0b7e566dbbb1284c564ddb9144ccf980be2c7e3
zoujie <zj89011810@163.com>
2022-07-09 17:43:33 +0800

    cell

---
+++ MyCodeApp/TemplateCode/VC/LeftAndRightListVC/View/ZJ_RightTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_RightTableViewCell.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/9.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_RightTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/TemplateCode/VC/LeftAndRightListVC/View/ZJ_RightTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ZJ_RightTableViewCell.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/9.
+//
+
+#import "ZJ_RightTableViewCell.h"
+
+@implementation ZJ_RightTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
