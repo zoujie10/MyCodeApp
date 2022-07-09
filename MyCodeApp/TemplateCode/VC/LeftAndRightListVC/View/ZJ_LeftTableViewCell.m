abfbf113180ca5c86006382df3fbe02c120c789e
zoujie <zj89011810@163.com>
2022-07-09 17:39:53 +0800

    vc

--- MyCodeApp/TemplateCode/VC/LeftAndRightListVC/View/ZJ_LeftTableViewCell.m
+++ MyCodeApp/TemplateCode/VC/LeftAndRightListVC/View/ZJ_LeftTableViewCell.m
@@ -8,16 +8,42 @@
 #import "ZJ_LeftTableViewCell.h"
 
 @implementation ZJ_LeftTableViewCell
-
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
+- (instancetype)initWithStyle:(UITableViewCellStyle)style
+			  reuseIdentifier:(NSString *)reuseIdentifier{
+	self = [super initWithStyle:style
+				reuseIdentifier:reuseIdentifier];
+	if (self) {
+//		self.contentView.backgroundColor = [UIColor colorWithHexStringMethod:@"#F7F7F7"];
+		[self creatUI];
+	}
+	return self;
 }
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
+-(void)creatUI{
+	
+	UIView *lineView = [[UIView alloc] init];
+	[self.contentView addSubview:lineView];
+	[lineView mas_makeConstraints:^(MASConstraintMaker *make) {
+		make.left.equalTo(self.contentView);
+		make.top.mas_equalTo(self.contentView.mas_top).offset(10);
+		make.bottom.mas_equalTo(self.contentView.mas_bottom).offset(-10);
+		make.width.mas_equalTo(3);
+	}];
+//	lineView.backgroundColor = [UIColor colorWithHexStringMethod:@"#FC556C"];
+	lineView.hidden = YES;
+	self.lineView = lineView;
+	
+	self.titleLabel = [[UILabel alloc]init];
+	self.titleLabel.font = [UIFont systemFontOfSize:13
+											 weight:UIFontWeightMedium];
+	self.titleLabel.textAlignment = NSTextAlignmentCenter;
+//	self.titleLabel.textColor = [UIColor colorWithHexStringMethod:@"#85868A"];
+	[self.contentView addSubview:self.titleLabel];
+	
+	[self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+		make.left.mas_equalTo(self.lineView.mas_right).offset(8);
+		make.centerY.mas_equalTo(self);
+		make.right.mas_equalTo(self.contentView.mas_right);
+	}];
 }
 
 @end
