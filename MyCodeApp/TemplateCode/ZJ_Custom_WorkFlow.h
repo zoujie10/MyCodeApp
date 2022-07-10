c50cf0f91fcc5f8524bb53b2ed97727c6e397060
zoujie <zj89011810@163.com>
2022-07-10 21:06:57 +0800

    logic

--- MyCodeApp/TemplateCode/ZJ_Custom_WorkFlow.h
+++ MyCodeApp/TemplateCode/ZJ_Custom_WorkFlow.h
@@ -14,11 +14,19 @@ NS_ASSUME_NONNULL_BEGIN
 2/8原则，现在先把工作时间压缩到一天的20%-30%时间里，也就是4小时以内。剩余的时间，思考副业，提升综合技能，锻炼身体。
  
 1.有数据结构流程
+ 1.1 URL
+  1.2 ViewModel Request
+ 1.3 ViewModel Logic
+ 1.4 UI
+ 1.5 Config Data To UI
  
  
 2.无数据结构流程
- 
- 
+  1.1 UI  - Dead Data
+  1.2 ViewModel - Dead Data - Logic
+    1.3 URL
+ 1.4 ViewModel Reuqest
+ 1.5 ViewModel Logic
  */
 @interface ZJ_Custom_WorkFlow : NSObject
 
