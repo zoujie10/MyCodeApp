56561ee20df9d7bb9cb3778f05db18caa8cfeed6
zoujie <zj89011810@163.com>
2022-07-12 21:35:47 +0800

    logic

---
+++ MyCodeApp/TemplateCode/Model/ZJ_DataDicModel.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_DataDicModel.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_DataDicModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/TemplateCode/Model/ZJ_DataDicModel.m
@@ -0,0 +1,12 @@
+//
+//  ZJ_DataDicModel.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/12.
+//
+
+#import "ZJ_DataDicModel.h"
+
+@implementation ZJ_DataDicModel
+
+@end
