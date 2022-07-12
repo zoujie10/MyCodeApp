41786496532d9a75906a365d3d71656cbd4fa5b3
zoujie <zj89011810@163.com>
2022-07-12 21:47:43 +0800

    logic

---
+++ MyCodeApp/TemplateCode/Model/ZJ_DataStrModel.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_DataStrModel.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_DataStrModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/TemplateCode/Model/ZJ_DataStrModel.m
@@ -0,0 +1,12 @@
+//
+//  ZJ_DataStrModel.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/12.
+//
+
+#import "ZJ_DataStrModel.h"
+
+@implementation ZJ_DataStrModel
+
+@end
