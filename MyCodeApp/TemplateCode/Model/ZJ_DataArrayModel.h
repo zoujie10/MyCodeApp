3915f2b639a378b30f2206950b832813dc7f7504
zoujie <zj89011810@163.com>
2022-07-12 21:31:28 +0800

    logic

---
+++ MyCodeApp/TemplateCode/Model/ZJ_DataArrayModel.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_DataArrayModel.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_DataArrayModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/TemplateCode/Model/ZJ_DataArrayModel.m
@@ -0,0 +1,12 @@
+//
+//  ZJ_DataArrayModel.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/12.
+//
+
+#import "ZJ_DataArrayModel.h"
+
+@implementation ZJ_DataArrayModel
+
+@end
