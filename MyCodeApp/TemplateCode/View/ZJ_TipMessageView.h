aa9dcb7bcf218fc49604d51b9b318b0d8e889636
zoujie <zj89011810@163.com>
2022-07-14 17:40:34 +0800

    model

---
+++ MyCodeApp/TemplateCode/View/ZJ_TipMessageView.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_TipMessageView.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/14.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_TipMessageView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/TemplateCode/View/ZJ_TipMessageView.m
@@ -0,0 +1,20 @@
+//
+//  ZJ_TipMessageView.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/14.
+//
+
+#import "ZJ_TipMessageView.h"
+
+@implementation ZJ_TipMessageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
