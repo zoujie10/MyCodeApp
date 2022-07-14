aabc16136ea4e66c73ffe6f53a650c691ee2a17a
zoujie <zj89011810@163.com>
2022-07-14 20:48:18 +0800

    model

--- MyCodeApp/TemplateCode/View/ZJ_TipMessageView.h
+++ MyCodeApp/TemplateCode/View/ZJ_TipMessageView.h
@@ -8,9 +8,12 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
+typedef void(^Click_Button_Blcok)(void);
 
 @interface ZJ_TipMessageView : UIView
-
+@property (nonatomic , strong)UILabel *contentLabel;
+@property (nonatomic , strong)UIButton *click_Btn;
+@property (nonatomic , copy) Click_Button_Blcok click_block;
 @end
 
 NS_ASSUME_NONNULL_END
--- MyCodeApp/TemplateCode/View/ZJ_TipMessageView.m
+++ MyCodeApp/TemplateCode/View/ZJ_TipMessageView.m
@@ -9,12 +9,61 @@
 
 @implementation ZJ_TipMessageView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame
+{
+	self = [super initWithFrame:frame];
+	if (self) {
+		self.backgroundColor = ZJRgbColor(255, 251, 216);
+		[self creatUI];
+	}
+	return self;
 }
-*/
+-(void)creatUI{
+	[self addSubview:self.click_Btn];
+	[self addSubview:self.contentLabel];
+	
+	[self.contentLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+		make.centerY.mas_equalTo(self);
+		make.left.mas_equalTo(self).offset(10);
+		make.right.mas_equalTo(self).offset(-10);
+	}];
+	
+	[self.click_Btn mas_makeConstraints:^(MASConstraintMaker *make) {
+		make.centerY.mas_equalTo(self.contentLabel);
+		make.right.mas_equalTo(self.mas_right).offset(-10);
+	}];
+}
+
+- (UILabel *)contentLabel{
+	if (!_contentLabel) {
+		_contentLabel = [[UILabel alloc] init];
+		_contentLabel.textColor = ZJRgbColor(252, 126, 57);
+		_contentLabel.backgroundColor = [UIColor clearColor];
+		_contentLabel.textAlignment = NSTextAlignmentCenter;
+		_contentLabel.font = [UIFont boldSystemFontOfSize:12];
+		_contentLabel.layer.masksToBounds = YES;
+		_contentLabel.numberOfLines = 0;
+		_contentLabel.text = @"如您需要退货 请联系客服";
+	}
+	return _contentLabel;
+}
+- (UIButton *)click_Btn{
+	if (!_click_Btn) {
+		_click_Btn = [UIButton buttonWithType:UIButtonTypeCustom];
+		[_click_Btn setTitle:@"联系客服" forState:UIControlStateNormal];
+//		[_click_Btn setImage:[UIImage alloc] forState:UIControlStateNormal];
+		[_click_Btn setBackgroundColor:[UIColor clearColor]];
+		[_click_Btn setTitleColor:ZJRgbColor(252, 126, 57) forState:UIControlStateNormal];
+		_click_Btn.titleLabel.font = [UIFont boldSystemFontOfSize:12];
+		[_click_Btn addTarget:self action:@selector(alterButtonSelector:) forControlEvents:UIControlEventTouchUpInside];
+		[_click_Btn setImageToRight];
+	}
+	return _click_Btn;
+}
+-(void)alterButtonSelector:(UIButton *)sender{
+	if(self.click_block){
+		self.click_block();
+	}
+}
 
 @end
