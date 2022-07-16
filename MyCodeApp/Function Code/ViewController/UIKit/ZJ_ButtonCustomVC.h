681ee92f1d059bc218388f6bf15f8ca71de7af2c
zoujie <zj89011810@163.com>
2022-07-16 17:40:24 +0800

    view

---
+++ MyCodeApp/Function Code/ViewController/UIKit/ZJ_ButtonCustomVC.h
@@ -0,0 +1,16 @@
+//
+//  ZJ_ButtonCustomVC.h
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/16.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJ_ButtonCustomVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCodeApp/Function Code/ViewController/UIKit/ZJ_ButtonCustomVC.m
@@ -0,0 +1,31 @@
+//
+//  ZJ_ButtonCustomVC.m
+//  MyCodeApp
+//
+//  Created by Zoujie on 2022/7/16.
+//
+
+#import "ZJ_ButtonCustomVC.h"
+
+@interface ZJ_ButtonCustomVC ()
+
+@end
+
+@implementation ZJ_ButtonCustomVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
